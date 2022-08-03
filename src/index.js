ec6e3ea3285486f2736d91cfbe6952e08be3e53f
irinbutina <1irinbutina@gmail.com>
2022-08-03 16:48:08 +0700

    feat: solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,36 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = [] 
+    for(let i=0; i < expr.length; i +=10) {
+       let el = expr.slice(i, i+10); 
+        arr.push(el)
+    }
+ 
+    let words = []
+   
+    arr.forEach(elem => {
+        let str = ''
+        if (elem === '**********') { 
+            str = str + ' '
+            words.push(str)
+        } else { 
+            for(let j=0; j < elem.length; j +=2) {
+                let el = elem.slice(j, j+2); 
+                 if (el === '00') {
+                    str = str + ''
+                 } 
+                 if (el === '10') {
+                    str = str + '.'
+                 }
+                 if (el === '11') {
+                    str = str + '-'
+                 }
+             }
+        words.push(MORSE_TABLE[str])
+        }
+    })
+    return words.join('')
 }
 
 module.exports = {
